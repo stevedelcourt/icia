2bddc01e908e85e4a7d03f24519b2bb61a482b14
Steven Delcourt <steven.delcourt@mentivis.com>
2026-02-18 13:26:14 +0100

    Make homepage gradient top lighter

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
         </div>
       </section>
 
-      <main id="main-content" className="relative z-30 bg-gradient-to-b from-[#E5E4DF] to-white">
+      <main id="main-content" className="relative z-30 bg-gradient-to-b from-[#F5F4F0] to-white">
         <Section spacing="normal">
           <FadeIn delay={0.1}>
             <div className="max-w-2xl">
