f092b9611baa510464eb29e25b5f16144ef725c1
Steven Delcourt <steven.delcourt@mentivis.com>
2026-04-23 16:19:13 +0200

    Hero button: light green bg, navy text, darker green hover

--- app/page.tsx
+++ app/page.tsx
@@ -221,7 +221,7 @@ export default function Home() {
                 </p>
                 <Link
                   href="/#acteurs"
-                  className="inline-block px-8 py-4 text-lg text-white bg-[#00255D] hover:bg-black transition-colors duration-200"
+                  className="inline-block px-8 py-4 text-lg text-[#00255D] bg-[#bdf5ab] hover:bg-[#a8e6a0] transition-colors duration-200 font-semibold"
                 >
                   C&apos;est pour qui ?
                 </Link>
